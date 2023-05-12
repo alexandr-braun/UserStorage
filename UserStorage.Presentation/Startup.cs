6aeb20dd52c87901b2f1f59bf138c75707b81369
alexandr-braun <alexandr.braun.ru@gmail.com>
2023-05-12 19:24:04 +0300

    Fix program & startup

---
+++ UserStorage.Presentation/Startup.cs
@@ -0,0 +1,38 @@
+using UserStorage.Presentation.Consumers.User;
+
+namespace UserStorage.Presentation;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        services.AddHostedService<UserKafkaBatchConsumer>();    
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+    }
+}
