9ad9c12fec063c6e74bd884acc631e18fea500eb
alexandr-braun <alexandr.braun.ru@gmail.com>
2023-05-12 19:03:02 +0300

    Add kafka options

---
+++ UserStorage.Presentation/Options/KafkaOptions.cs
@@ -0,0 +1,10 @@
+namespace UserStorage.Presentation.Options;
+
+public class KafkaOptions
+{
+    public string BootstrapServers { get; set; }
+    public string GroupId { get; set; }
+    public string Topic { get; set; }
+    public int BatchSize { get; set; }
+    public int BatchTimeout { get; set; }
+}
